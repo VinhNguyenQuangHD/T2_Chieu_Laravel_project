57f1c5ce3458cc849fb2944090060753eaca1ea1
VinhNguyenQuangHD <61536993+VinhNguyenQuangHD@users.noreply.github.com>
2022-03-01 21:36:37 +0700

    change new

--- ChieuT2_2022_laravel_project/app/Http/Controllers/CustomAuth.php
+++ ChieuT2_2022_laravel_project/app/Http/Controllers/CustomAuth.php
@@ -6,5 +6,11 @@ use Illuminate\Http\Request;
 
 class CustomAuth extends Controller
 {
-    //
+    public function login(){
+        return view("auth.login");
+    }
+
+    public function register(){
+        return view("auth.login");
+    }
 }
